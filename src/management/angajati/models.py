c2be04e10db8061c76954edede0e7ed06c785db8
Dan-Lucian <dlnita@jouve.com>
2022-08-01 11:31:10 +0300

    feat: Finished model documentation

--- src/management/angajati/models.py
+++ src/management/angajati/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import IntegerField, CharField
+
+
+class Angajat(models.Model):
+
+    nume = CharField(max_length=250)
+    prenume = CharField(max_length=250)
+    varsta = IntegerField()
+    oras = CharField(max_length=250)
